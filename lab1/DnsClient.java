4ceb983a8e7190701c63a033ab853453981a2ede
Dylan Havelock <dylan.havelock@gmail.com>
2019-09-22 18:53:55 -0400

    input error check

--- lab1/DnsClient.java
+++ lab1/DnsClient.java
@@ -1,3 +1,4 @@
+
 /**
  * DNS Client
  * 
@@ -9,8 +10,7 @@ import java.net.*;
 import java.util.Random;
 
 public class DnsClient {
-	public static void main(String args[]) throws Exception
-	{
+    public static void main(String args[]) throws Exception {
         // Random number generator
         Random rand = new Random();
         short queryId = (short) rand.nextInt(Short.MAX_VALUE + 1);
@@ -18,14 +18,14 @@ public class DnsClient {
         // default arguments
         int timeout = 5;
         int maxRetries = 3;
-        int port = 53; 
+        int port = 53;
         String domainName = "";
         String server = "";
         short qType = 0x0001;
         String qTypeStr = "A";
-		
-		// Create byte array for IP address
-        byte[] ipAddressByte = new byte[]{0, 0, 0, 0};
+
+        // Create byte array for IP address
+        byte[] ipAddressByte = new byte[] { 0, 0, 0, 0 };
 
         // Byte arrays for send and receive data
         byte[] sendData = new byte[1024];
@@ -69,9 +69,15 @@ public class DnsClient {
                 else if (args[i].substring(1, 3).equals("ns")) {
                     qType = 0x0002;
                     qTypeStr = "NS";
+                } 
+
+                // incorrect input error
+                else {
+                    System.out.println("ERROR\tIncorrect input format. Use: java DnsClient [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name");
+                    System.exit(1);
                 }
-            } 
-            
+            }
+
             // DNS server ip
             else if (args[i].substring(0, 1).equals("@")) {
                 args[i] = args[i].substring(1);
@@ -86,6 +92,12 @@ public class DnsClient {
 
                 domainName = args[i];
             }
+
+            // incorrect input error
+            else {
+                System.out.println("ERROR\tIncorrect input format. Use: java DnsClient [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name");
+                System.exit(1);
+            }
         }
 
         InetAddress ipDns = InetAddress.getByAddress(ipAddressByte);
@@ -97,35 +109,44 @@ public class DnsClient {
         System.out.println("Request type: " + qTypeStr);
 
         // Create a UDP socket
-		DatagramSocket clientSocket = new DatagramSocket(1024);
-        int attempts;
-        long startTime = 0; 
+        DatagramSocket clientSocket = new DatagramSocket(1024);
+        int attempts = 0;
+        long startTime = 0;
         long endTime = 0;
 
         // Create and Send Packet
         for (attempts = 0; attempts < maxRetries; attempts++) {
-        
+
             sendPacket = new DatagramPacket(sendData, sendData.length, ipDns, port);
             clientSocket.send(sendPacket);
             receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.setSoTimeout(timeout*1000);
-            
+            clientSocket.setSoTimeout(timeout * 1000);
+
             try {
                 startTime = System.currentTimeMillis();
                 clientSocket.receive(receivePacket);
                 endTime = System.currentTimeMillis();
             } catch (Exception e) {
                 continue;
-            }    
-            
-           if (receivePacket != null) break;
+            }
+
+            if (receivePacket != null)
+                break;
         }
 
-        if (attempts == maxRetries){
+        // Close the socket
+        clientSocket.close();
+
+        if (attempts == maxRetries) {
             System.out.println("ERROR\tMaximum number of retries " + attempts + " exceeded");
             System.exit(1);
-        } 
+        }
 
+        processResponsePacket(receivePacket, sendPacket, endTime, startTime, attempts);
+    }
+
+    public static void processResponsePacket(DatagramPacket receivePacket, DatagramPacket sendPacket, double endTime,
+            double startTime, int attempts) throws IOException {
         if (receivePacket == null) {
             System.out.println("ERROR\tFailed to connect to DNS Server");
         } else {
@@ -144,43 +165,21 @@ public class DnsClient {
             short nscount = dataIn.readShort();
             short arcount = dataIn.readShort();
 
-            if(ra == 0) {
-                System.out.println("ERROR\tThe server does not support recursive queries");
-            }
-            
-            switch(rcode){
-                case 0: 
-                    break;
-                
-                case 1:
-                    System.out.println("ERROR\tThe name server was unable to interpret the query");
-                    System.exit(1);
-
-                case 2: 
-                    System.out.println("ERROR\tThe name server was unable to process this query due to a problem with the name server");
-                    System.exit(2);
-
-                case 3:
-                    System.out.println("NOTFOUND");
-                    System.exit(3);
-
-                case 4:
-                    System.out.println("ERROR\tThe name server does not support the requested kind of query");
-                    System.exit(4);
-
-                case 5:
-                    System.out.println("ERROR\tThe name server refuses to perform the requested operation for policy reasons");
-                    System.exit(5);
-            }
-            
             inputStream.close();
             dataIn.close();
 
-            double duration = (endTime - startTime)/1000.0;
+            if (ra == 0) {
+                System.out.println("ERROR\tThe server does not support recursive queries");
+            }
+
+            processRCode(rcode);
+
+            double duration = (endTime - startTime) / 1000.0;
 
             System.out.println("Response receieved after " + duration + " seconds (" + attempts + " retries)");
             System.out.println("***Answer Section (" + ancount + " records)***");
 
+            // Parse Answer Records
             for (int record = 0; record < ancount; record++) {
                 answerIndex += parseRecord(receivePacket, answerIndex, aa);
             }
@@ -190,16 +189,41 @@ public class DnsClient {
                 answerIndex += getRecordLength(receivePacket, answerIndex);
             }
 
-            
             System.out.println("***Additional Section (" + arcount + " records)***");
 
+            // Parse Additional Records
             for (int record = 0; record < arcount; record++) {
                 answerIndex += parseRecord(receivePacket, answerIndex, aa);
             }
         }
+    }
 
-		// Close the socket
-		clientSocket.close();
+    public static void processRCode(int rcode) {
+        switch (rcode) {
+        case 0:
+            break;
+
+        case 1:
+            System.out.println("ERROR\tThe name server was unable to interpret the query");
+            System.exit(1);
+
+        case 2:
+            System.out.println(
+                    "ERROR\tThe name server was unable to process this query due to a problem with the name server");
+            System.exit(2);
+
+        case 3:
+            System.out.println("NOTFOUND");
+            System.exit(3);
+
+        case 4:
+            System.out.println("ERROR\tThe name server does not support the requested kind of query");
+            System.exit(4);
+
+        case 5:
+            System.out.println("ERROR\tThe name server refuses to perform the requested operation for policy reasons");
+            System.exit(5);
+        }
     }
 
     public static byte[] constructRequest(int queryId, int qType, String domainName) throws IOException {
@@ -208,11 +232,11 @@ public class DnsClient {
 
         // header
         dataOut.writeShort(queryId); // id
-        dataOut.writeShort(0x0100);  // line 2
-        dataOut.writeShort(0x0001);  // QDCOUNT
-        dataOut.writeShort(0x0000);  // ANCOUNT
-        dataOut.writeShort(0x0000);  // NSCOUNT
-        dataOut.writeShort(0x0000);  // ARCOUNT
+        dataOut.writeShort(0x0100); // line 2
+        dataOut.writeShort(0x0001); // QDCOUNT
+        dataOut.writeShort(0x0000); // ANCOUNT
+        dataOut.writeShort(0x0000); // NSCOUNT
+        dataOut.writeShort(0x0000); // ARCOUNT
 
         // QNAME
         String[] labels = domainName.split("\\."); // assuming max 10 labels
@@ -238,7 +262,8 @@ public class DnsClient {
     }
 
     public static int getRecordLength(DatagramPacket packet, int offset) throws IOException {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(packet.getData(), offset + 10, packet.getLength()-offset);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(packet.getData(), offset + 10,
+                packet.getLength() - offset);
         DataInputStream dataIn = new DataInputStream(inputStream);
 
         short dataLength = dataIn.readShort();
@@ -249,9 +274,10 @@ public class DnsClient {
 
         return recordLength;
     }
-    
+
     public static int parseRecord(DatagramPacket packet, int offset, int aa) throws IOException {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(packet.getData(), offset, packet.getLength()-offset);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(packet.getData(), offset,
+                packet.getLength() - offset);
         DataInputStream dataIn = new DataInputStream(inputStream);
 
         short name = dataIn.readShort();
@@ -259,46 +285,46 @@ public class DnsClient {
         short classIn = dataIn.readShort();
         int ttl = dataIn.readInt();
         short dataLength = dataIn.readShort();
-        
+
         int recordLength = 12 + dataLength;
 
-        if(classIn != 1){
+        if (classIn != 1) {
             System.out.println("ERROR\tUnexpected CLASS code");
             System.exit(1);
         }
 
         String auth = aa == 1 ? "auth" : "nonauth";
 
-        switch(type) {
-            case 0x0001: // A
-                int data0 = dataIn.readByte() & 0xff;
-                int data1 = dataIn.readByte() & 0xff;
-                int data2 = dataIn.readByte() & 0xff;
-                int data3 = dataIn.readByte() & 0xff;
-                System.out.println("IP\t" + data0 + "." + data1 + "." + data2 + "." + data3 + "\t " + ttl + "\t" + auth);
-                break;
+        switch (type) {
+        case 0x0001: // A
+            int data0 = dataIn.readByte() & 0xff;
+            int data1 = dataIn.readByte() & 0xff;
+            int data2 = dataIn.readByte() & 0xff;
+            int data3 = dataIn.readByte() & 0xff;
+            System.out.println("IP\t" + data0 + "." + data1 + "." + data2 + "." + data3 + "\t " + ttl + "\t" + auth);
+            break;
 
-            case 0x0002: // NS
-                offset += 12;
-                String nameNs = getName(packet, offset);
-                System.out.println("NS\t" + nameNs + "\t" + ttl + "\t" + auth);
-                break;
+        case 0x0002: // NS
+            offset += 12;
+            String nameNs = getName(packet, offset);
+            System.out.println("NS\t" + nameNs + "\t" + ttl + "\t" + auth);
+            break;
 
-            case 0x000f: // MX
-                short preference = dataIn.readShort();
-                offset += 14;
-                String nameMx = getName(packet, offset);
-                System.out.println("MX\t" + nameMx + "\t" + preference + "\t" + ttl + "\t" + auth);
-                break;
+        case 0x000f: // MX
+            short preference = dataIn.readShort();
+            offset += 14;
+            String nameMx = getName(packet, offset);
+            System.out.println("MX\t" + nameMx + "\t" + preference + "\t" + ttl + "\t" + auth);
+            break;
 
-            case 0x0005: // CNAME
-                offset += 12;
-                String nameCname = getName(packet, offset);
-                System.out.println("CNAME\t" + nameCname + "\t" + ttl + "\t" + auth);
-                break;
+        case 0x0005: // CNAME
+            offset += 12;
+            String nameCname = getName(packet, offset);
+            System.out.println("CNAME\t" + nameCname + "\t" + ttl + "\t" + auth);
+            break;
 
-            default:
-                System.out.println("ERROR");
+        default:
+            System.out.println("ERROR\tInvalid record type");
         }
 
         inputStream.close();
@@ -319,11 +345,11 @@ public class DnsClient {
                 }
                 num = pointerBytes[offset + index];
             } else if ((num & 0xc0) == 0xc0) { // check if pointer
-                offset = ((num & 0x0000003f) << 8) + (pointerBytes[offset + index] & 0xff );
+                offset = ((num & 0x0000003f) << 8) + (pointerBytes[offset + index] & 0xff);
                 num = pointerBytes[offset];
                 index = 0;
             } else {
-                name += (char)pointerBytes[offset + index];
+                name += (char) pointerBytes[offset + index];
                 num--;
             }
             index++;
